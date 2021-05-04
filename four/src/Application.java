c01d9e305f15b0bffc3b3b6fd4d0b9098dcb16c0
Amer Anwar <adaroobi@hotmail.com>
2021-05-04 08:48:26 +0300

    04.2 Objects in action

--- four/src/Application.java
+++ four/src/Application.java
@@ -1,5 +1,14 @@
 public class Application {
     public static void main(String[] args) {
+        Human ahmed = new Human();
+        ahmed.name = "Ahmed";
+        ahmed.age = 20;
 
+        Human fatima = new Human();
+        fatima.name = "Fatima";
+        fatima.age = 23;
+
+        System.out.println(ahmed.selfIntroduce());
+        System.out.println(fatima.selfIntroduce());
     }
 }
--- four/src/Human.java
+++ four/src/Human.java
@@ -3,6 +3,26 @@ public class Human {
     public int age;
 
     public String talk() {
-        return "Hi";
+        return "Hi! I am " + name + ", and I am " + age + " years old.";
+    }
+
+    public String selfIntroduce() {
+        return "Hi! I am " + name + ", and I am " + age + " years old.";
+    }
+
+    public void grow() {
+        age++;
+    }
+
+    public String sneeze() {
+        return "Atchooo";
+    }
+
+    public String cough() {
+        return "Cough Cough";
+    }
+
+    public String yawn() {
+        return  "Yaaaaaaaaaaawn";
     }
 }
