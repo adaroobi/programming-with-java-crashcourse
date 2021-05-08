fd80d119df03d9705bc5910d943ca3351f56ebba
Amer Anwar <adaroobi@hotmail.com>
2021-05-08 06:04:37 +0300

    04.7 References and Similar datatypes

--- four/src/Application.java
+++ four/src/Application.java
@@ -5,10 +5,40 @@ public class Application {
         Human mohamed = new Human("Mohamed", 1);
         Human sarah = Human.born("Sarah");
 
-        ahmed.talk();
-        fatima.talk();
-        mohamed.talk();
-        System.out.println(sarah.getAge());
-        sarah.talk();
+        Human ahmed2 = ahmed;
+
+        println(ahmed.toString());
+        println(ahmed2.toString());
+
+        ahmed2.setAge(50);
+
+        int x = 1;
+        int y = x;
+        y = 6;
+
+        String n = "String";
+        String m = n;
+        m = "Stringa";
+
+        println(ahmed.getAge());
+        println(y == x);
+        println(n == m);
+
+    }
+
+    private static void println(String text) {
+        System.out.println(text);
+    }
+
+    private static void println(int text) {
+        System.out.println(text);
+    }
+
+    private static void println(boolean text) {
+        System.out.println(text);
+    }
+
+    private static void println(double text) {
+        System.out.println(text);
     }
 }
