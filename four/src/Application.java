fe79c2a835d8c74fb2b0e3650115559719332b2e
Amer Anwar <adaroobi@hotmail.com>
2021-05-18 08:27:29 +0300

    session week6 recording

--- four/src/Application.java
+++ four/src/Application.java
@@ -1,5 +1,9 @@
 public class Application {
     public static void main(String[] args) {
+        // GUI Event Based
 
+        // Event loop
+        // Action Event (Trigger, Dispatcher)
+        // Event handler (Listener)
     }
 }
